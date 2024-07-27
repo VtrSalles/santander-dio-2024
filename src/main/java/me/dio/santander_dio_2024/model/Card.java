baf978f49c2181c047a361f0b1a2093579f6e0f8
Vitor Barbosa Salles <vitor@logstore.com.br>
2024-07-26 22:47:36 -0300

    Implementação da camada de dominio.

---
+++ src/main/java/me/dio/santander_dio_2024/model/Card.java
@@ -0,0 +1,39 @@
+package me.dio.santander_dio_2024.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+@Entity(name = "tb_card")
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private Integer number;
+    @Column(name = "available_limit",scale = 13, precision = 2)
+    private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
---
+++ src/main/java/me/dio/santander_dio_2024/model/News.java
@@ -0,0 +1,9 @@
+package me.dio.santander_dio_2024.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+
+}
