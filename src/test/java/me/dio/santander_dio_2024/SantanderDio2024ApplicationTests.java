5e0bac95411bfb26a911611dadf52717f5d9d297
Vitor Barbosa Salles <vitor@logstore.com.br>
2024-07-26 21:39:10 -0300

    first commit

---
+++ src/test/java/me/dio/santander_dio_2024/SantanderDio2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dio_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDio2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
